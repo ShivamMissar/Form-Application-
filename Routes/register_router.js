a9e2bf6ce5b501064aa0f6f25549bb9b51e4dd86
Shivam <N0949022@ntu.ac.uk>
2024-02-28 13:54:33 +0000

    Corrected register controller functionaility

--- Routes/register_router.js
+++ Routes/register_router.js
@@ -1,13 +1,14 @@
 // routes/registerRouter.js
 const express = require('express');
+const { registerUser } = require('../Controller/registerController');
 const router = express.Router();
 
+// GET request to render the registration form
 router.get('/', (req, res) => {
   res.sendFile('register.html', { root: './views' });
 });
 
-
-router.post('/', )
-
+// POST request to handle user registration
+router.post('/', registerUser); // pass the function reference without calling it
 
 module.exports = router;
