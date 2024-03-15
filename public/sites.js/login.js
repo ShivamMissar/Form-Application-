2a8c589712b0de09d178e33e16fb9ab23064f908
Shivam <N0949022@ntu.ac.uk>
2024-03-15 11:40:32 +0000

    Added styling for error messages on register and login form

--- public/sites.js/login.js
+++ public/sites.js/login.js
@@ -1,24 +1,39 @@
 
 
-
-
-
-function validate_form()
+function validate_form_username()
 {
     // gets the values from the form within the login by id.
     let login_username = document.getElementById('username').value; 
-    let login_password = document.getElementById('password').value;    
-    const error = document.getElementById('errormessage');
+    
+    const error = document.getElementById('errormessage-username');
 
 
-    if(login_username == '' || login_password == '')
+    if(login_username == '')
     {
-        error.innerHTML = "Username/password field is blank";
+        error.innerHTML = "Username field is blank";
         return false; // will not allow form submission to happen
     }
 
     return true; // will allow the form submission to happen as the fields are now non blank
 
 }
+
+function validate_form_password()
+{
+    let login_password = document.getElementById('password').value; 
+    const error = document.getElementById('errormessage-password');
+
+
+    if(login_password == '')
+    {
+        error.innerHTML = "password field is blank";
+        return false; // will not allow form submission to happen
+    }
+
+    return true; // will allow the form submission to happen as the fields are now non blank
+}
 // call the function 
-validate_form();
+document.getElementById('username').addEventListener('input', validate_form_username);
+document.getElementById('password').addEventListener('input', validate_form_password);
+
+
