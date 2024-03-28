efb96b5adae3306313ad0ae7f409e66dc45f259c
Shivam <N0949022@ntu.ac.uk>
2024-03-28 13:08:22 +0000

    Fixed admin link issue

--- public/sites.js/header.js
+++ public/sites.js/header.js
@@ -13,11 +13,15 @@
                 document.getElementById('trendingTags').style.display = 'block';
             
 
-                const checkAdmin = data.getUserInformation.Username == 'Admin';
-                if(checkAdmin)
-                {
-                    document.getElementById('adminLink').style.display = 'block';
-                }
+                const isAdmin = data.getUserInformation.Username === 'Admin';
+                console.log('Is admin:', isAdmin);
+            if (isAdmin) {
+                console.log('User is admin. Showing admin link.');
+                document.getElementById('adminLink').style.display = 'block';
+            } else {
+                console.log('User is not admin. Hiding admin link.');
+                document.getElementById('adminLink').style.display = 'none';
+            }
             
 
                 
